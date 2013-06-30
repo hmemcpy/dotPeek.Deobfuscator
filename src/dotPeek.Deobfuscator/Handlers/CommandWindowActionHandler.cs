558e10c5a2e562dd9b6b52b2e7c3a5effabeb245
Igal Tabachnik <hmemcpy@gmail.com>
2013-07-01 00:41:14 +0300

    Implemented deobfuscation. Buttons needed.

--- src/dotPeek.Deobfuscator/Handlers/CommandWindowActionHandler.cs
+++ src/dotPeek.Deobfuscator/Handlers/CommandWindowActionHandler.cs
@@ -1,21 +1,141 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Forms;
 using JetBrains.ActionManagement;
+using JetBrains.Annotations;
+using JetBrains.Application;
 using JetBrains.Application.DataContext;
-using System.Diagnostics;
+using JetBrains.Application.Progress;
+using JetBrains.Application.Settings;
+using JetBrains.Application.Settings.Store.Implementation;
+using JetBrains.IDE.TreeBrowser;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Impl;
+using JetBrains.ProjectModel.Model2.Assemblies.Interfaces;
+using JetBrains.ProjectModel.Model2.References;
+using JetBrains.ReSharper.Feature.Services.ExternalSources.CSharp.AssemblyExport;
+using JetBrains.ReSharper.Features.Browsing.AssemblyExplorer;
+using JetBrains.ReSharper.Features.Browsing.AssemblyExplorer.ExplorerNodesModel.Core;
+using JetBrains.ReSharper.Features.Browsing.AssemblyExplorer.ExplorerNodesModel.Nodes;
+using JetBrains.ReSharper.Features.Browsing.AssemblyExplorer.ProjFileModel.Export;
+using JetBrains.ReSharper.Features.Browsing.AssemblyExplorer.ProjFileModel.Export.History;
+using JetBrains.TreeModels;
+using JetBrains.UI.Application.Progress;
+using JetBrains.UI.CrossFramework;
+using JetBrains.UI.Settings;
+using JetBrains.Util;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace dotPeek.Deobfuscator
 {
     [ActionHandler("dotPeek.Deobfuscator.CommandWindowAction")]
     public class CommandWindowActionHandler : IActionHandler
     {
-        public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nexUpdate)
+        public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
         {
-            // return true/false to enable/disable this action
-            return true;
+            ISolution solution;
+            return TryGetExistingAssemblyFile(context, out solution) != null;
         }
-        
+
         public void Execute(IDataContext context,DelegateExecute nextExecute)
         {
-            Process.Start("cmd");
-        }        
+            ISolution solution;
+            IAssemblyFile existingAssemblyFile = TryGetExistingAssemblyFile(context, out solution);
+            if (existingAssemblyFile == null)
+                return;
+
+            var deobfuscator = solution.TryGetComponent<IAssemblyDeobfuscatorManager>();
+            if (deobfuscator == null)
+                return;
+            
+            string newFileName = AskUser(existingAssemblyFile);
+            if (newFileName == null)
+                return;
+
+            FileSystemPath newAssembly = null;
+            Shell.Instance.GetComponent<UITaskExecutor>().FreeThreaded.ExecuteTask("Deobfuscating...", TaskCancelable.Yes, progressIndicator =>
+            {
+                using (ReadLockCookie.Create())
+                    newAssembly = deobfuscator.Execute(existingAssemblyFile, newFileName, progressIndicator);
+            });
+
+            if (newAssembly != null)
+            {
+                AddToAssemblyExplorer(newAssembly, solution);
+            }
+        }
+
+        private string AskUser(IAssemblyFile existingAssemblyFile)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                OverwritePrompt = true,
+                FileName = existingAssemblyFile.Location.NameWithoutExtension + "-deobfuscated" + existingAssemblyFile.Location.ExtensionWithDot,
+                DefaultExt = "dll",
+                Filter = "DLL File (*.dll)|*.dll|All Files (*.*)|*.*",
+            };
+
+            using (saveFileDialog)
+            {
+                if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                    return null;
+
+                return saveFileDialog.FileName;
+            }
+        }
+
+        private static void AddToAssemblyExplorer(FileSystemPath assemblyPath, ISolution solution)
+        {
+            var component = solution.TryGetComponent<IAssemblyExplorerManager>();
+            if (component == null)
+                return;
+
+            component.AddItemsByPath(new[] { assemblyPath });
+        }
+
+
+        private static IAssemblyFile TryGetExistingAssemblyFile(IDataContext context, out ISolution solution)
+        {
+            solution = context.GetData(JetBrains.ProjectModel.DataContext.DataConstants.SOLUTION);
+            if (solution == null)
+                return null;
+            IList<TreeModelNode> data = context.GetData(TreeModelBrowser.TREE_MODEL_NODES);
+            if (data == null || data.Count != 1)
+                return null;
+            IAssemblyFile assemblyFile = GetAssemblyFile(data.First());
+            if (assemblyFile == null)
+                return null;
+            AssemblyInfoCache component = solution.TryGetComponent<AssemblyInfoCache>();
+            if (component == null)
+                return null;
+            if (!assemblyFile.Location.ExistsFile || AssemblyExplorerUtil.AssemblyIsBroken(assemblyFile.Location, component))
+                return null;
+            
+            return assemblyFile;
+        }
+
+        private static IAssemblyFile GetAssemblyFile(TreeModelNode assemblyNode)
+        {
+            IAssemblyFile assemblyFile = null;
+            var assemblyFileNode = assemblyNode.DataValue as IAssemblyFileNode;
+            if (assemblyFileNode != null)
+            {
+                assemblyFile = assemblyFileNode.GetAssemblyFile();
+            }
+            else
+            {
+                var assemblyReferenceNode = assemblyNode.DataValue as AssemblyReferenceNode;
+                if (assemblyReferenceNode != null)
+                {
+                    IAssembly assemblyResolveResult = assemblyReferenceNode.Reference.GetModuleToAssemblyResolveResult();
+                    if (assemblyResolveResult != null)
+                        assemblyFile = assemblyResolveResult.GetFiles().FirstOrDefault();
+                }
+            }
+
+            return assemblyFile;
+        }
     }
 }
---
+++ src/dotPeek.Deobfuscator/IAssemblyDeobfuscatorManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using de4dot.code;
+using de4dot.code.AssemblyClient;
+using de4dot.code.deobfuscators;
+using de4dot.code.renamer;
+using dnlib.DotNet;
+using JetBrains.Application;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel.Model2.Assemblies.Interfaces;
+using JetBrains.Util;
+
+namespace dotPeek.Deobfuscator
+{
+    public interface IAssemblyDeobfuscatorManager
+    {
+        IEnumerable<IDeobfuscatorInfo> KnownDeobfuscators { get; }
+        FileSystemPath Execute(IAssemblyFile existingAssemblyFile, string newFileName, IProgressIndicator progressIndicator);
+    }
+
+    [ShellComponent]
+    public class AssemblyDeobfuscatorManager : IAssemblyDeobfuscatorManager
+    {
+        private static readonly IEnumerable<IDeobfuscatorInfo> knownDeobfuscators = new IDeobfuscatorInfo[]
+        {
+            new de4dot.code.deobfuscators.Unknown.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Agile_NET.DeobfuscatorInfo(), // aka CliSecure
+            new de4dot.code.deobfuscators.Babel_NET.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.CodeFort.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.CodeVeil.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.CodeWall.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.CryptoObfuscator.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.DeepSea.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Dotfuscator.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.dotNET_Reactor.v3.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.dotNET_Reactor.v4.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Eazfuscator_NET.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Goliath_NET.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.ILProtector.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.MaxtoCode.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.MPRESS.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Rummage.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Skater_NET.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.SmartAssembly.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Spices_Net.DeobfuscatorInfo(),
+            new de4dot.code.deobfuscators.Xenocode.DeobfuscatorInfo(),
+        };
+
+        public IEnumerable<IDeobfuscatorInfo> KnownDeobfuscators
+        {
+            get { return knownDeobfuscators; }
+        }
+
+        public FileSystemPath Execute(IAssemblyFile existingAssemblyFile, string newFileName, IProgressIndicator progressIndicator)
+        {
+            var context = new ModuleContext();
+            var fileOptions = new ObfuscatedFile.Options
+            {
+                Filename = existingAssemblyFile.Location.FullPath,
+                NewFilename = newFileName,
+            };
+
+            IObfuscatedFile obfuscationFile = CreateObfuscationFile(fileOptions, context);
+
+            return Deobfuscate(obfuscationFile, progressIndicator);
+        }
+
+        private FileSystemPath Deobfuscate(IObfuscatedFile obfuscatedFile, IProgressIndicator progressIndicator)
+        {
+            try
+            {
+                progressIndicator.Start(2);
+                progressIndicator.TaskName = "Deobfuscating...";
+                progressIndicator.CurrentItemText = string.Format("Saving to {0}", obfuscatedFile.NewFilename);
+
+                obfuscatedFile.deobfuscateBegin();
+
+                obfuscatedFile.deobfuscate();
+
+                obfuscatedFile.deobfuscateEnd();
+
+                progressIndicator.Advance(1);
+                // turn all flags on
+                RenamerFlags flags = RenamerFlags.DontCreateNewParamDefs | RenamerFlags.DontRenameDelegateFields |
+                                     RenamerFlags.RenameEvents |
+                                     RenamerFlags.RenameFields | RenamerFlags.RenameGenericParams |
+                                     RenamerFlags.RenameMethodArgs |
+                                     RenamerFlags.RenameMethods | RenamerFlags.RenameNamespaces |
+                                     RenamerFlags.RenameProperties |
+                                     RenamerFlags.RenameTypes | RenamerFlags.RestoreEvents |
+                                     RenamerFlags.RestoreEventsFromNames |
+                                     RenamerFlags.RestoreProperties | RenamerFlags.RestorePropertiesFromNames;
+                var renamer = new Renamer(obfuscatedFile.DeobfuscatorContext, new[] { obfuscatedFile }, flags);
+                renamer.rename();
+
+                progressIndicator.Advance(1);
+                obfuscatedFile.save();
+            }
+            finally
+            {
+                progressIndicator.Stop();
+            }
+
+            return new FileSystemPath(obfuscatedFile.NewFilename);
+        }
+
+        private IObfuscatedFile CreateObfuscationFile(ObfuscatedFile.Options fileOptions, ModuleContext moduleContext)
+        {
+            var obfuscatedFile = new ObfuscatedFile(fileOptions, moduleContext, new NewAppDomainAssemblyClientFactory())
+            {
+                DeobfuscatorContext = new DeobfuscatorContext()
+            };
+
+            try
+            {
+                obfuscatedFile.load(knownDeobfuscators.Select(info => info.createDeobfuscator()).ToList());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.ShowError(ex.Message);
+                return null;
+            }
+            return obfuscatedFile;
+        }
+    }
+}
